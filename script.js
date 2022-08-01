af4758f4b9508e3199a071fa44ebb7c3c8240299
TONY160896 <tonycmathew84@gmail.com>
2022-08-01 11:53:45 +0530

    full project complte

---
+++ script.js
@@ -0,0 +1,10 @@
+function onClickRight() {
+     var slider=document.getElementById("slider")
+     slider.scrollLeft=slider.scrollRight+180
+}
+                    
+function onClickLeft() {
+     var slider=document.getElementById("slider")
+     slider.scrollLeft=slider.scrollLeft+180
+}
+     
